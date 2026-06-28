3b7f1e1e1990d3cfeb35d64872d4af414881ed5b
Warre van der Maat <60901879+warrevdm@users.noreply.github.com>
2026-06-28 09:33:06 +0200

    Load high contrast white TV dashboard theme

--- dashboard.php
+++ dashboard.php
@@ -7,6 +7,7 @@
     <link rel="stylesheet" href="assets/style.css">
     <link rel="stylesheet" href="assets/pro-benchmarks.css">
     <link rel="stylesheet" href="assets/concept-slide.css">
+    <link rel="stylesheet" href="assets/tv-light.css">
 
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
